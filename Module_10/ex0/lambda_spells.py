ce2dc20c5fac48431fd0a1de349b36f9a4bdcc2c
manferre <manferre@student.42lisboa.com>
2026-05-26 16:49:16 +0100

    Module_10 ex0, ex1, ex2 done

---
+++ Module_10/ex0/lambda_spells.py
@@ -0,0 +1,64 @@
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    return sorted(artifacts,
+                  key=lambda artifact: artifact["power"], reverse=True)
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    return list(filter(lambda mage: mage["power"] >= min_power, mages))
+
+
+def spell_transformer(spells: list[str]) -> list[str]:
+    return list(map(lambda spell: "* " + spell + " *", spells))
+
+
+def mage_stats(mages: list[dict]) -> dict:
+    powers: list[int] = list(map(lambda mage: mage["power"], mages))
+
+    max_power: int = max(mages, key=lambda mage: mage["power"])["power"]
+    min_power: int = min(mages, key=lambda mage: mage["power"])["power"]
+    avg_power: float = round(sum(powers) / len(powers), 2)
+
+    return {
+        "max_power": max_power,
+        "min_power": min_power,
+        "avg_power": avg_power
+    }
+
+
+def main() -> None:
+
+    artifacts: list[dict] = []
+    mages: list[dict] = []
+    spells: list[str] = []
+
+    try:
+        print("Testing artifact sorter...")
+
+        sorted_artifacts: list[dict] = artifact_sorter(artifacts)
+        sorted_string: str = ""
+        for i in range(len(sorted_artifacts)):
+            sorted_string += (f"{sorted_artifacts[i]["name"]} "
+                              f"({sorted_artifacts[i]["power"]} power)")
+            if i == 0:
+                sorted_string += " comes before "
+            elif i > 0 and i < len(sorted_artifacts) - 1:
+                sorted_string += " which comes before "
+        print(sorted_string)
+
+        print("\nTesting power filter...")
+        print(power_filter(mages, 60))
+        print("\nTesting spell transformer...")
+        print(spell_transformer(spells))
+        print("\nTesting mage stats computation...")
+        print(mage_stats(mages))
+    except (KeyError, ValueError, ZeroDivisionError) as e:
+        print(f"Error: {e}\nCheck the structure of the data being used:"
+              "\n(1) artifacts should be a list of dict: {'name': str, "
+              "'power': int, 'type': str}"
+              "\n(2) mages should be a list of dict: {'name': str,"
+              " 'power': int, 'element': str}"
+              "\n(3) spells should be a list of str")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Module_10/ex1/higher_magic.py
@@ -0,0 +1,100 @@
+from collections.abc import Callable
+
+
+Spell = Callable[[str, int], str]
+
+
+def heal(target: str, power: int) -> str:
+    return f"Heal restores {target} for {power} HP"
+
+
+def fireball(target: str, power: int) -> str:
+    return f"Fireball hits {target} for {power} HP"
+
+
+def shield(target: str, power: int) -> str:
+    return f"Shield protects {target} for {power} HP"
+
+
+def spell_combiner(spell1: Spell, spell2: Spell) -> Callable[[str, int],
+                                                             tuple[str, str]]:
+    if not callable(spell1) or not callable(spell2):
+        raise TypeError("Both arguments must be spells")
+
+    def combined(target: str, power: int) -> tuple[str, str]:
+        return (spell1(target, power), spell2(target, power))
+
+    return combined
+
+
+def power_amplifier(base_spell: Spell, multiplier: int) -> Spell:
+    if not callable(base_spell):
+        raise TypeError("base_spell musst be a spell")
+
+    def amplified(target: str, power: int) -> str:
+        return (base_spell(target, power * multiplier))
+
+    return amplified
+
+
+def conditional_caster(condition: Callable[[int], bool],
+                       spell: Spell) -> Spell:
+    if not callable(condition) or not callable(spell):
+        raise TypeError("condition must be a callable function that"
+                        " returns bool. spell must be a spell")
+
+    def cast(target: str, power: int) -> str:
+        if condition(power):
+            return spell(target, power)
+        return "Spell fizzled"
+
+    return cast
+
+
+def spell_sequence(spells: list[Spell]) -> Callable[[str, int], list[str]]:
+    if not all(callable(spell) for spell in spells):
+        raise TypeError("All elements in spells must be spells")
+
+    def sequence(target: str, power: int) -> list[str]:
+        return [spell(target, power) for spell in spells]
+
+    return sequence
+
+
+def main() -> None:
+
+    test_targets: list[str] = ["Dragon", "Goblin", "Wizard", "Knight"]
+    test_values: list[int] = [18, 15, 21]
+    try:
+        print("\nTesting spell combiner...")
+        combined_spell = spell_combiner(fireball, shield)
+        result = combined_spell(test_targets[0], test_values[0])
+        print(f"Combined spell result: {result[0].split(sep="for")[0]},"
+              f" {result[1].split(sep="for")[0]}")
+
+        print("\nTesting power amplifier...")
+        amplified_spell = power_amplifier(shield, 2)
+        result_amplified = amplified_spell(test_targets[1], test_values[1])
+        print(f"Original: {test_values[1]}, "
+              f"Amplified: {"".join(filter(str.isdigit, result_amplified))}")
+
+        print("\nTesting conditional caster...")
+        conditional_spell = conditional_caster(lambda power: power > 20, heal)
+        result_conditional_1 = conditional_spell(test_targets[2],
+                                                 test_values[2])
+        result_conditional_2 = conditional_spell(test_targets[2],
+                                                 test_values[1])
+        print(f"Testing with power > 20 - {result_conditional_1}")
+        print(f"Testing with power < 20 - {result_conditional_2}")
+
+        print("\nTesting spell_sequence...")
+        sequence_spell = spell_sequence([heal, fireball, shield])
+        result_sequence = sequence_spell(test_targets[3], test_values[0])
+        print(result_sequence)
+
+    except TypeError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
